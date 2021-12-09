93856077028fa0d5d9cf08f28b3c3d4cfb12df3f
MascotYehorKovalov <yehor.kovalov1@nure.ua>
2021-12-09 21:54:02 +0200

    Models

---
+++ ContactList/ContactList/Models/Contact.cs
@@ -0,0 +1,9 @@
+﻿namespace ContactList.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string FullName => $"{Name} {Surname}";
+    }
+}
